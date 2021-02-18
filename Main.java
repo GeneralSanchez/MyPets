225c51e300ed6375e04f84ba8f924d99a095b058
Andy Sanchez <sanchezzandy206@gmail.com>
2021-02-18 00:37:08 +0000

    Updated the addOwnerBranch

--- Main.java
+++ Main.java
@@ -6,16 +6,39 @@ public  class Main
 
     Dog piper = new Dog("Piper", true, "black and white", 10, 0.5, false, 8, "Australian Shepherd");
 
+    PetParent vader = new Dog("Vader", true, "brown", 4 , 0.02, false, 1, "Sheepdog");
+
     Axolotl matthew = new Axolotl("Matthew", false, "pink", 5, 0.6, true);
+
+    PetParent clifford = new Axolotl("Clifford", true, "purple", 10, 0.01, false);
+
+    Owner emiliano = new Owner("Emiliano");
+    System.out.println(emiliano.toString());
+
+    System.out.println(emiliano.feed(piper));
+    System.out.println(emiliano.feed(matthew));
+    System.out.println(emiliano.feed(vader));
+
+    
 
     // blankPet.setName("Fluffy");
     // System.out.println("Bob is now called " + blankPet.getName());
+/*
+    System.out.println(vader.dailyRoutine());
+    System.out.println(vader.speak());
+    System.out.println(((Dog)vader).getLoyalty());
+    System.out.println(clifford.dailyRoutine());
+    PetParent[] myPets = {piper, vader, matthew, clifford};
+    for(PetParent animals : myPets)
+    {
+      System.out.println(animals.getName() + " says " +animals.speak() + ".");
+    }
+*/
+    /*
     System.out.println(piper.dailyRoutine());
     System.out.println(matthew.dailyRoutine());
-
     System.out.println("Piper says " + piper.speak());
     System.out.println("Matthew says " + matthew.speak());
-
 /*
     System.out.println(blankPet.toString());
     System.out.println("\n---------------------\n");
